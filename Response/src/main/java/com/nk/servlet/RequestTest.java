7789c9fae7ffdc9f53e1f491c341a9cd79a3ab7a
7xiao <2082334514@qq.com>
2023-03-22 16:33:07 +0800

    redirect重定向 request请求的测试

---
+++ Response/src/main/java/com/nk/servlet/RequestTest.java
@@ -0,0 +1,24 @@
+package com.nk.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class RequestTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        System.out.println("进入RequestTest请求\n用户名:"+username+"\n密码："+password);
+        resp.sendRedirect("/r/success.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
