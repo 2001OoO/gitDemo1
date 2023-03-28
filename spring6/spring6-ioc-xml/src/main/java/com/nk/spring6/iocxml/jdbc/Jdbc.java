95c15d1045b316618dc2de508666b1490812c625
6xiao <773674495@qq.com>
2023-03-29 01:25:54 +0800

    spring bean 引入外部属性文件

---
+++ spring6/spring6-ioc-xml/src/main/java/com/nk/spring6/iocxml/jdbc/Jdbc.java
@@ -0,0 +1,25 @@
+package com.nk.spring6.iocxml.jdbc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Jdbc {
+
+    public void demo1(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/demo1db?profileSQL=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+    }
+
+    @Test
+    public void demo2(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean-jdbc.xml");
+        DruidDataSource druidDataSource = (DruidDataSource) applicationContext.getBean("druidDataSource");
+        System.out.println(druidDataSource.getUrl());
+    }
+}
