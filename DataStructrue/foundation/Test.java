36c3ca4c43e2d5965ea7713b138e07904f0df9fa
6xiao <773674495@qq.com>
2023-03-30 15:45:10 +0800

    Study Test

---
+++ DataStructrue/foundation/Test.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+//        float f = 1111.4111111f;
+//        double s = 1111.4111328125222222D;
+//        System.out.println((double) f);
+//        System.out.println(s);
+
+//        long s = 1234567891234L;
+//        double s1 = s;
+//        System.out.println(s1);
+
+//        Scanner reader = new Scanner(System.in);
+//        Byte next = reader.nextByte();
+//        System.out.println(next);
+
+        int a[] = {1,2};
+        int b[] = {1,2,3,4,5};
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("**************************");
+        a = b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
